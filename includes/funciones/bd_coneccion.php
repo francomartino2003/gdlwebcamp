cff4114fca6510ffa29fdaa54237bca40992bd6b
francomartino2003 <ch16.martino@gmail.com>
2020-10-26 19:34:19 -0500

    a

--- includes/funciones/bd_coneccion.php
+++ includes/funciones/bd_coneccion.php
@@ -1,3 +1,3 @@
 <?php
- $conn = new mysqli('us-cdbr-east-02.cleardb.com','ba8aad5c19744e','franco2003','heroku_666607da59d6f3f');
+ $conn = new mysqli('us-cdbr-east-02.cleardb.com','ba8aad5c19744e','e91141c7','heroku_666607da59d6f3f');
  ?>
