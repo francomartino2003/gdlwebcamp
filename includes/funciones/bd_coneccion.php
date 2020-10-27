0a454cfd5864cfb610a3df322e6dfab2f33c60bd
francomartino2003 <ch16.martino@gmail.com>
2020-10-26 19:27:58 -0500

    last commit

--- includes/funciones/bd_coneccion.php
+++ includes/funciones/bd_coneccion.php
@@ -1,3 +1,3 @@
 <?php
- $conn = new mysqli('localhost','root','150403','GDLWebCamp');
+ $conn = new mysqli('us-cdbr-east-02.cleardb.com','ba8aad5c19744e','franco2003','heroku_666607da59d6f3f');
  ?>
